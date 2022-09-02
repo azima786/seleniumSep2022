24b3d721c9c5d9efad23ac5a9503def9ff2effc4
Azima Keshwani <azima786@gmail.com>
2022-09-02 16:05:45 -0500

    update

--- selenium/src/test/java/test/java/com/selenium/test.java
+++ selenium/src/test/java/test/java/com/selenium/test.java
@@ -1,5 +1,8 @@
 package test.java.com.selenium;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -15,8 +18,19 @@ public class test {
 		driver.findElement(By.name("inputPassword")).sendKeys("123");
 		driver.findElement(By.className("submit")).click();
 		//implicit wait
+		driver.manage().timeouts().implicitlyWait(100000, TimeUnit.SECONDS);
 		driver.findElement(By.cssSelector("p.error")).getText();
+		driver.findElement(By.linkText("Forgot your password?")).click();
+		driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("John");
+		driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("John@rsa.com");
+		driver.findElement(By.xpath("//input[@type='text'][2]")).clear();
+		driver.findElement(By.cssSelector("input[type='text']:nth-child(3)")).sendKeys("john@gmail.com");
+		driver.findElement(By.xpath("//form/input[3]")).sendKeys("2057961234");
+		Thread.sleep(3000);
+		driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+		driver.findElement(By.cssSelector("form p")).getText();
 
+driver.quit();
 
 	}
 }
