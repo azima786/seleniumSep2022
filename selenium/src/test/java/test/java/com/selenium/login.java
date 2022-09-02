65d07b5c6da69595089da3705d4e67849383e251
Azima Keshwani <azima786@gmail.com>
2022-09-02 16:41:31 -0500

    update

---
+++ selenium/src/test/java/test/java/com/selenium/login.java
@@ -0,0 +1,20 @@
+package test.java.com.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class login {
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "selenium/target/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		driver.findElement(By.cssSelector("#inputUsername")).sendKeys("rahul");
+		driver.findElement(By.cssSelector("input[type*='pass']")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.id("chkboxOne")).click();
+		driver.findElement(By.xpath("//button[contains(@class, 'submit')]")).click();
+
+		
+	}
+}
--- selenium/src/test/java/test/java/com/selenium/test.java
+++ selenium/src/test/java/test/java/com/selenium/test.java
@@ -18,7 +18,7 @@ public class test {
 		driver.findElement(By.name("inputPassword")).sendKeys("123");
 		driver.findElement(By.className("submit")).click();
 		//implicit wait
-		driver.manage().timeouts().implicitlyWait(100000, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(100000, TimeUnit.SECONDS); //Something to show
 		driver.findElement(By.cssSelector("p.error")).getText();
 		driver.findElement(By.linkText("Forgot your password?")).click();
 		driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("John");
@@ -26,9 +26,16 @@ public class test {
 		driver.findElement(By.xpath("//input[@type='text'][2]")).clear();
 		driver.findElement(By.cssSelector("input[type='text']:nth-child(3)")).sendKeys("john@gmail.com");
 		driver.findElement(By.xpath("//form/input[3]")).sendKeys("2057961234");
-		Thread.sleep(3000);
+		Thread.sleep(3000); //Page to be displayed
 		driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
-		driver.findElement(By.cssSelector("form p")).getText();
+		System.out.println(driver.findElement(By.cssSelector("form p")).getText());
+		driver.findElement(By.xpath("//div[@class='forgot-pwd-btn-conainer']/button[1]")).click();
+		driver.findElement(By.cssSelector("#inputUsername")).sendKeys("rahul");
+		driver.findElement(By.cssSelector("input[type*='pass']")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.id("chkboxOne")).click();
+		driver.findElement(By.xpath("//button[contains(@class, 'submit')]")).click();
+
+
 
 driver.quit();
 
