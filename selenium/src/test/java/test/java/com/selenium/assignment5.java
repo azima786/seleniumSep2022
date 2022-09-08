dd70dbe914d69ef968380be15e969b4ae6b89878
Azima Keshwani <azima786@gmail.com>
2022-09-08 15:26:25 -0500

    update

---
+++ selenium/src/test/java/test/java/com/selenium/assignment5.java
@@ -0,0 +1,39 @@
+package test.java.com.selenium;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class assignment5 {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "selenium/target/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
+		driver.get("https://the-internet.herokuapp.com/");
+
+		driver.findElement(By.linkText("Nested Frames")).click();
+		driver.switchTo().frame("frame-top");
+
+		driver.switchTo().frame("frame-middle");
+
+		System.out.println(driver.findElement(By.id("content")).getText());
+
+
+
+	
+
+		
+
+driver.close();
+
+
+
+	}
+}
---
+++ selenium/src/test/java/test/java/com/selenium/scope.java
@@ -0,0 +1,28 @@
+package test.java.com.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class scope {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "selenium/target/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
+		driver.get("https://www.rahulshettyacademy.com/AutomationPractice/");
+
+		
+	
+
+		
+
+driver.close();
+
+
+
+	}
+}
