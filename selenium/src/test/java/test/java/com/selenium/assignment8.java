0195858a97dfea9842af9f247ca8cef3cf736262
Azima Keshwani <azima786@gmail.com>
2022-09-09 14:12:57 -0500

    update

---
+++ selenium/src/test/java/test/java/com/selenium/assignment8.java
@@ -0,0 +1,30 @@
+package test.java.com.selenium;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class assignment8 {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "selenium/target/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
+		String name = "ind";
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		driver.findElement(By.id("autocomplete")).sendKeys(name);
+		Thread.sleep(2000);
+		driver.findElement(By.id("autocomplete")).sendKeys(Keys.DOWN);
+		driver.findElement(By.id("autocomplete")).sendKeys(Keys.DOWN);
+
+		System.out.println(driver.findElement(By.id("autocomplete")).getAttribute("value")); //When you are unable to get values using send keys
+		
+		}
+		}
+
+
+	
