2fe67c992996aa423f193730661224dbafb2f41b
Azima Keshwani <azima786@gmail.com>
2022-09-13 09:14:45 -0500

    update

---
+++ selenium/src/test/java/test/java/com/TestNG/day1.java
@@ -0,0 +1,17 @@
+package test.java.com.TestNG;
+
+import org.testng.annotations.Test;
+
+public class day1 {
+
+    @Test
+public void name() {
+    System.out.println("HELLO");
+}
+
+@Test
+public void name2() {
+    System.out.println("HELLO");
+}
+
+}
