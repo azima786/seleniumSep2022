e807f05eceff02f12e77d048ff5922f4438f0bf8
Azima Keshwani <azima786@gmail.com>
2022-09-14 14:52:54 -0500

    update

--- selenium/src/test/java/test/java/com/test.java
+++ selenium/src/test/java/test/java/com/test.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class test {
@@ -31,46 +33,51 @@ public class test {
 		// }
 	
 	//How to use Lambda Expressions
-//		 Long C = names.stream().filter(s -> s.startsWith("A")).count();
-//		System.out.println(C);
-//names.stream().filter(s->s.length()>4.forEach (s-> System.out.println(s)) - prints length of 4
-//names.stream().filter(s ->s.length()>4).limit(1).forEach(s->System.out.println(s)); // Scan through each array and print in 1 shot
+		 Long C = names.stream().filter(s -> s.startsWith("A")).count();
+		System.out.println(C);
+names.stream().filter(s->s.length()>4).forEach (s-> System.out.println(s)); //- prints length of 4
+names.stream().filter(s ->s.length()>4).limit(1).forEach(s->System.out.println(s)); // Scan through each array and print in 1 shot
 		}
 	
 
 	public static void streamMap() {
+ArrayList<String> names = new ArrayList<String>();
+names.add("man");
+names.add("Don");
+names.add("Women");
 
 		//Mapping lowercase to upperCase
-	// 	Stream.of("Abhijeeet", "Don", "Alekhya", "Adam", "rama").filter(s->s.endswith("a").map(s->s.toUpperCase()) //filter results and provide results
-	// 	.forEach(s->System.out.println(s));
-	// }
+		Stream.of("Abhijeeet", "Don", "Alekhya", "Adam", "rama").filter(s->s.endsWith("a'")).map(s->s.toUpperCase()).forEach(s->System.out.println(s));
+	
 
 
 	// //print names with first letter as a uppercase and sorted
 
-	// //Arrays.asList() - Converts into List
-	// List<String> names = Arrays.asList("Abhijeeet", "Don", "Alekhya", "Adam", "rama");
-	// names.stream().filter(s->s.startsWith("a")).sorted().map(s->s.toUpperCase()).forEach(s->System.out.println(s));
-// 	ArrayList<String> names = new ArrayList<String>();
-// 	names.add("Abhi");
-// 	names.add("Donny");
-// 	names.add("Aleja");
-// 	names.add("Saumya");
-// 	names.add("Rahul");
+	//Arrays.asList() - Converts into List
 
-// 	List<String> names1 = Arrays.asList("Abhijeeet", "Don", "Alekhya", "Adam", "rama");
-// 	Stream <String> newStream =Stream.concat(names.stream(), names1.stream());
-// newStream.sorted().forEach(s->System.out.println(s));
+	
+	List<String> namenew = Arrays.asList("Abhijeeet", "Don", "Alekhya", "Adam", "rama");
+	namenew.stream().filter(s->s.startsWith("a")).sorted().map(s->s.toUpperCase()).forEach(s->System.out.println(s));
+	ArrayList<String> name = new ArrayList<String>();
+	name.add("Abhi");
+	name.add("Donny");
+	name.add("Aleja");
+	name.add("Saumya");
+	name.add("Rahul");
 
-// boolean flag = newStream.anyMatch(s->s.equalsIgnoreCase("Adam"));//Just return if condition is true or false
-// Assert.assertTrue(flag);
+	List<String> names1 = Arrays.asList("Abhijeeet", "Don", "Alekhya", "Adam", "rama");
+	Stream <String> newStream =Stream.concat(names.stream(), names1.stream());
+newStream.sorted().forEach(s->System.out.println(s));
+
+boolean flag = newStream.anyMatch(s->s.equalsIgnoreCase("Adam"));//Just return if condition is true or false
+Assert.assertTrue(flag);
 
 	}
 
 @Test
 	public static void streamCollect() {
-// List<String> ls=	Stream.of("Abhijeeet", "Don", "Alekhya", "Adam", "rama").filter(s->s.endswith("a").map(s->s.toUpperCase()).collect(Collectors.toList());
-// 	ls.get(0);
+List<String> ls=	Stream.of("Abhijeeet", "Don", "Alekhya", "Adam", "rama").filter(s->s.endsWith("a")).map(s->s.toUpperCase()).collect(Collectors.toList());
+	ls.get(0);
 
 	List<Integer> values = Arrays.asList(3,2,2,7,5,1,9,7);
 values.stream().distinct().forEach(s->System.out.println(s));
